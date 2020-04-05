20acb3fd185106531f20b100f5b10c29d0a00446
talhasaglam <talhasaglam153@gmail.com>
2020-04-05 14:25:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/popopmenu/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.popopmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                PopupMenu popup = new PopupMenu(MainActivity.this,button);
+                popup.getMenuInflater().inflate(R.menu.pop_up_menu,popup.getMenu());
+
+
+
+                popup.show();
+
+
+            }
+        });
+
+    }
+}
